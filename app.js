67874cccf1c88c14ea34a3bfaa90bc269a7ba6f9
d-akhil-kumar <akillcoder@gmail.com>
2021-06-18 11:00:30 +0530

    basic graphql

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res,next)=> {
-    res.send('Welcome To a GraphQL Demo Application')
-})
+const {ApolloServer, gql} = require('apollo-server-express')
+
+
+//1. define schemas here
+const typeDefs = gql`  
+  type Query {
+      hello : String
+  }
+`
+
+//2. write resolvers for your schemas
+const resolvers = {
+    Query : {
+        hello: () => { return 'hello graphQL world'}
+    }
+}
+
+
+//3. Initialize the ApolloExpressServer
+const server = new ApolloServer({typeDefs, resolvers})
+
+//4. set apollo server on top of express app
+server.applyMiddleware({app})
+
 
 
 const port = 3300
-
 app.listen(port, () => {
     console.log('server is running on port ' + port)
+    console.log(`Default ApolloGQL server path ${server.graphqlPath}`)
 })

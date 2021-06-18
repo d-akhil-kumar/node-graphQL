59a21b76997ad405df187ef4c91fe85b29ddf2fc
d-akhil-kumar <akillcoder@gmail.com>
2021-06-18 15:32:17 +0530

    employee query

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const app = express()
 
 const {ApolloServer, gql} = require('apollo-server-express')
 
+const employeesData = require('./data/employees')
+
 
 //1. define schemas here
-const typeDefs = gql`  
+const typeDefs = gql`
+
+  type Employee {
+      id: ID!
+      firstName: String
+      lastName: String
+      jobLevel: Int
+  }
+
   type Query {
       hello : String,
-      randomNumber: Int
+      randomNumber: Int,
+      employees: [Employee]
   }
 `
 
@@ -18,6 +29,9 @@ const resolvers = {
         hello: () => { return 'hello graphQL world'},
         randomNumber: () => {
             return Math.floor(Math.random() * 10) + 1
+        },
+        employees: () => {
+            return employeesData
         }
     }
 }
---
+++ data/employees.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      id: "E1000",
+      firstName: "James",
+      lastName: "Ulrich",
+      jobLevel: 4,
+    },
+    {
+      id: "E1001",
+      firstName: "Robert",
+      lastName: "Hetfield",
+      jobLevel: 3,
+    },
+  ];
